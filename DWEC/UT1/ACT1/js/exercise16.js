aa84e6bb42be47f4542fe7e7a0598834f9f646bd
vctorr1 <vctorr1@gmail.com>
2024-09-30 13:12:05 +0200

    commit dia clase

--- DWEC/UT1/ACT1/js/exercise16.js
+++ DWEC/UT1/ACT1/js/exercise16.js
@@ -1,4 +1,13 @@
-/**16. Función para dibujar un patrón de diente de sierra inverso */
+/**16. Crea una función para dibujar un patrón de diente de sierra inverso en un cuadro de texto.
+Con un carácter y un número que indique el mayor número de caracteres en la base (inversa)
+del patrón.
+Ejemplo 1. Datos de entrada: 'A' y 5
+AAAAA
+AAAA
+AAA
+AA
+A
+s */
 
 "use strict"
 {
